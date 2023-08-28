ee0ba1fac687aa3f338c457d08259f154ea99504
Denis <denis.vishchak@gmail.com>
2023-08-28 15:31:48 +1000

    fix change case;

--- ex 5/change_case.php
+++ ex 5/change_case.php
@@ -4,16 +4,14 @@ $color = array('A' => 'Blue', 'B' => 'Green', 'C' => 'Red');
 changeRegex($color, 'strtolower');
 print_r($color);
 
-echo '<br>';
-
 changeRegex($color, 'strtoupper');
 print_r($color);
 
-function changeRegex($array, $function)
+function changeRegex(&$array, $function)
 {
     foreach ($array as $key => $value) {
         if (!empty($value)) {
-            $array[$key] = $function($value);
+            $array[$key] = call_user_func($function, $value);
         }
     }
 }
